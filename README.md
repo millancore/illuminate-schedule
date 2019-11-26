08c308a442218cfeabfc9db7f920baec8a2a6da7
Juan Millan <juanes14569@hotmail.com>
2019-11-25 21:01:32 -0300

    Split Kernel

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # illuminate-schedule
 Use laravel Schedule in non-Laravel applications
+
+## How to use.
+
+Clone this proyect.
+
+```
+git clone https://github.com/millancore/illuminate-schedule.git 
+```
+
+Install dependencies
+
+```
+composer install
+```
+ Add your commands and task into `scr/Kernel.php` in the same way as Laravel
+
+ [Laravel Task Scheduling](https://laravel.com/docs/5.8/scheduling)
+
+ ## Enjoy!
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,55 +2,28 @@
 
 namespace Schedule;
 
-use Illuminate\Console\Application as Artisan;
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Console\Scheduling\ScheduleRunCommand;
-use Illuminate\Events\Dispatcher;
 
-class Kernel
+class Kernel extends ConsoleKernel
 {
-    protected $app;
-    protected $artisan;
-    protected $events;
-
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
     protected $commands = [
-        #Register commads here!
+        //
     ];
 
-    public function __construct(Application $app, Dispatcher $events)
-    {
-        $this->app = $app;
-        $this->events = $events;
-    }
-
-    public function handle($input, $output = null)
-    {
-        $this->bootstrap();
-        return $this->getArtisan()->run($input, $output);
-    }
-
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
     protected function schedule(Schedule $schedule)
     {
-        #Add to schedule taks here!
-    }
-
-    protected function bootstrap()
-    {
-        $schedule = new Schedule();
-
-        $this->getArtisan()->add(
-            new ScheduleRunCommand($schedule)
-        );
-
-        $this->schedule($schedule);
-    }
-
-    protected function getArtisan()
-    {
-        if (is_null($this->artisan)) {
-            return $this->artisan = (new Artisan($this->app, $this->events, '5.8'))
-                ->resolveCommands($this->commands);
-        }
-        return $this->artisan;
+        // $schedule->command('inspire')
+        //          ->hourly();
     }
 }
